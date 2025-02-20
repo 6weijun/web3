6253a34b9cd3f8d592adb7eec201a9965e4e8910
luweijun <luweij@gmail.com>
2025-02-21 00:38:41 +0800

    init

--- contracts/StudentStorage.sol
+++ contracts/StudentStorage.sol
@@ -2,16 +2,24 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract StudentStorage {
-    string name;
-    uint age;
-    
-    function setDate(string memory _name, uint _age) public{
-      name = _name;
-      age = _age;
+    struct Student {
+        uint id;
+        string name;
+        uint age;
+    }
+
+    //动态数组
+    Student[] public studentList;
+
+    function addList(string memory _name, uint _age) public returns (uint) {
+        uint count = studentList.length;
+        uint id = count + 1;
+        studentList.push(Student(id, _name, _age, msg.sender));
+        return studentList.length;
     }
 
     //view 视图函数，只访问不修改状态, pure 纯函数，不访问也不修改
-    function getDate() public view returns (string memory, uint){
-      return(name, age);
+    function getDate() public view returns (Student[] memory) {
+        return studentList;
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import addContent from './add-content.js';
 import { version, exchanges } from 'ccxt';
 import Web3 from 'web3';
+import abi from './build/contracts/StudentStorage.json';
 
 var web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
 
@@ -21,16 +22,32 @@ async function init() {
   // const chinId = await web3.eth.getChainId();
   // const idkey = await web3.eth.requestAccounts();
   // console.log('idkey', idkey);
-  
-  
+
   showBalance('0x70997970C51812dc3A010C7d01b50e0d17dc79C8');
 
-  await web3.eth.sendTransaction({
-    from: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
-    to: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-    value: web3.utils.toWei(1, 'ether')
-  })
+  // await web3.eth.sendTransaction({
+  //   from: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
+  //   to: '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
+  //   value: web3.utils.toWei(1, 'ether')
+  // })
   showBalance('0x70997970C51812dc3A010C7d01b50e0d17dc79C8');
+
+  //连接合约
+  const contact_address = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
+  const studentStorage = new web3.eth.Contract(abi.abi, contact_address);
+
+  const getList = async function () {
+    const studentList = await studentStorage.methods.getDate().call();
+    console.log('studentStorage2:', studentList);
+  }
+  const kk = meg.sender();
+  debugger;
+  getList()
+  const data = await studentStorage.methods.addList('luqiushi', 39).send({
+    from: '0x70997970C51812dc3A010C7d01b50e0d17dc79C8'
+  });
+  console.log('studentStorage', data);
+  getList()
 }
 
 init();
